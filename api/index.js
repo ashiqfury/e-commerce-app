6e3008f90b7911e934c7d3a524391223bec5f30d
Fury <ashiqasraf07@gmail.com>
2021-10-11 18:56:58 +0530

    Added stripe routes

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
-
-dotenv.config();
+const stripeRoute = require('./routes/stripe');
 const app = express();
 
 mongoose
@@ -15,11 +15,14 @@ mongoose
 	.then(() => console.log('Database connected successfully!'))
 	.catch((err) => console.log('Connection Failed', err));
 
+dotenv.config();
+app.use(cors());
 app.use(express.json()); // accepts json format in post request
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 app.listen(process.env.PORT || 8080, () => console.log('Server running on http://localhost:2506'));
