90291e4cbde38285037443c3ca5a7ca0893acb9d
Fury <ashiqasraf07@gmail.com>
2021-10-16 16:34:37 +0530

    completed admin dashboard widget

--- api/models/User.js
+++ api/models/User.js
@@ -6,6 +6,7 @@ const UserSchema = new mongoose.Schema(
 		email: { type: String, required: true, unique: true },
 		password: { type: String, required: true },
 		isAdmin: { type: Boolean, default: false },
+		img: { type: String },
 	},
 	{ timestamps: true }
 );
--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:2506/api';
-const TOKEN =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNjE2MWE3MGVkYjdjMDlmMzg0MmEzYiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNDAxNzY2NywiZXhwIjoxNjM0Mjc2ODY3fQ.dBRPKFt6DjhAXDtN_K6y_fXSUKI91cHynSYx-5WAWso';
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser
+	.accessToken;
 
 export const publicRequest = axios.create({
 	baseURL: BASE_URL,
